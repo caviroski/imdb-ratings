c39ec78f82f2d94b3636373d73088dcc62d16a2f
Igor Caviroski <icavir@gmail.com>
2025-09-30 14:53:18 +0200

    optimize

--- frontend/imdb-ratings-frontend/src/components/SelectDate.jsx
+++ frontend/imdb-ratings-frontend/src/components/SelectDate.jsx
@@ -4,7 +4,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-export default function SelectDate({ value = "", onChange, label = "Select Date", options = [], disabled = false, ...props }) {
+export default function SelectDate({ value = "", onChange, label = "Select Date", options = [], ...props }) {
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
@@ -15,7 +15,6 @@ export default function SelectDate({ value = "", onChange, label = "Select Date"
           value={value}
           label={label}
           onChange={onChange}
-          disabled={disabled}
           inputProps={{ 'data-testid': 'test-select-date' }}
           {...props}
         >
