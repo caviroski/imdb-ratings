76f1c6c1fb6a8d9d46b7ee652f73450b2c0b9142
Igor Caviroski <icavir@gmail.com>
2025-09-30 13:58:39 +0200

    fix server run

--- frontend/imdb-ratings-frontend/vite.config.js
+++ frontend/imdb-ratings-frontend/vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  publicDir: '../public',
   server: {
     port: 3000,
     open: true
   },
   build: {
-    outDir: '../dist'
+    outDir: 'dist'
   },
   test: {
     globals: true,
