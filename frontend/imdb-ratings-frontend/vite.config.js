a33228cc408983327655d1ccb51015d3067f42f4
Igor Caviroski <icavir@gmail.com>
2025-09-30 13:49:49 +0200

    fix results.json

--- frontend/imdb-ratings-frontend/vite.config.js
+++ frontend/imdb-ratings-frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './src',
+  root: '.',
   publicDir: '../public',
   server: {
     port: 3000,
